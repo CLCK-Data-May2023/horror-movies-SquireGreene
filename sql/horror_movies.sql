a74684a3a281a94626fc4acb2c41991c71d7627b
squire greene <squiregr@gmail.com>
2023-10-15 10:47:38 -0400

    Replaced "Movie Title" with "Movie_Title"

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,4 +1,4 @@
-SELECT id AS "Movie_ID", name AS "Movie Title", imdb_rating AS "Rating" 
+SELECT id AS "Movie_ID", name AS "Movie_Title", imdb_rating AS "Rating" 
 FROM movies 
 WHERE year <= 1985
 AND genre = "horror"
